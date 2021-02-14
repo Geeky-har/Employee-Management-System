bb59a50e46147ab0602945079acc7a2dedc44b70
Harsh negi <harshnegi6477@gmail.com>
2021-02-14 12:18:45 +0530

    added bootstrap alerts

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .forms import EmployeeForm
 from .models import Employee
+from django.contrib import messages
 
 # Create your views here.
 
 def employee_form(request, id=0):
     if request.method == 'GET':
 
-        if id == 0: # means new registration page is running    
+        if id == 0: # means new registration page is running
             form = EmployeeForm()
 
         else:   # means update needs to take place(to fill the existing form)
@@ -24,10 +25,14 @@ def employee_form(request, id=0):
 
         if id == 0: # new registration page is running
 
+            isUpdate = False
+
             # will store the details of the form fields
             form = EmployeeForm(request.POST)
 
         else:   # means update needs to take place
+
+            isUpdate = True
 
             # will return the object containing the specified id
             employee = Employee.objects.get(pk=id)
@@ -39,6 +44,14 @@ def employee_form(request, id=0):
 
         if form.is_valid():
             form.save() # will save the details to the db
+
+            # block for alert
+
+            if isUpdate:
+                messages.success(request, 'Record is Successfully updated')
+
+            else:
+                messages.success(request, 'New record is successfully added')
 
         return redirect('/employee/list')
 
